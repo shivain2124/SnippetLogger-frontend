e5412f036e72ea629dd8874c5efb2ce68ceeba45
shivain2124 <shivainsharma2124@gmail.com>
2025-05-27 00:59:41 +0530

    reverted back to 26 may midnight

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  define: {
-    global: 'globalThis'
-  },
-  optimizeDeps: {
-    include: ['axios']
-  }
+  plugins: [react(),tailwindcss()],
 })
